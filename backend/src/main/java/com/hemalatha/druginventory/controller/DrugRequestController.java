6f5a419f6fdc1e8824d86bfe7c308b81b42ab435
Hemalatha-mani <mhemalatharmanivel@gmail.com>
2026-07-19 13:32:41 +0530

    Integrate EmailService for drug request submissions
    Added email notification for new drug requests.

--- backend/src/main/java/com/hemalatha/druginventory/controller/DrugRequestController.java
+++ backend/src/main/java/com/hemalatha/druginventory/controller/DrugRequestController.java
@@ -2,6 +2,7 @@ package com.hemalatha.druginventory.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,13 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hemalatha.druginventory.entity.DrugRequest;
 import com.hemalatha.druginventory.repository.DrugRequestRepository;
-
+import com.hemalatha.druginventory.service.EmailService;
 @RestController
 @RequestMapping("/api/drugrequests")
 @CrossOrigin(origins = "*")
 public class DrugRequestController {
 
+
     private final DrugRequestRepository repository;
+    @Autowired
+    private EmailService emailService;    
 
     public DrugRequestController(
             DrugRequestRepository repository) {
@@ -33,15 +37,28 @@ public class DrugRequestController {
     }
 
     @PostMapping
-    public DrugRequest add(
-            @RequestBody DrugRequest request) {
-        if (request == null) {
+public DrugRequest add(
+        @RequestBody DrugRequest request) {
+
+    if (request == null) {
         throw new RuntimeException(
             "Request data cannot be null");
     }
 
-        return repository.save(request);
-    }
+    DrugRequest savedRequest = repository.save(request);
+
+    emailService.sendEmail(
+        "bloodbank.admin@gmail.com",
+        "New Drug Request Submitted",
+        "Hospital : " + savedRequest.getHospital()
+        + "\nMedicine : " + savedRequest.getDrug()
+        + "\nRequested Quantity : " + savedRequest.getRequestedQty()
+        + "\nRequested Date : " + savedRequest.getRequestedDate()
+        + "\n\nA new drug request has been submitted."
+    );
+
+    return savedRequest;
+}
 
     @PutMapping("/{id}")
     public DrugRequest update(
@@ -51,6 +68,7 @@ public class DrugRequestController {
         request.setRequestId(id);
 
         return repository.save(request);
+        
     }
 
     @DeleteMapping("/{id}")
